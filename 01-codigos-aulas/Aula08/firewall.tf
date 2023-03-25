bd1be6ba28d9912825e19e64151fcae66c556e85
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-03-25 16:51:44 -0300

    exemplos aula 8

--- 01-codigos-aulas/Aula08/firewall.tf
+++ 01-codigos-aulas/Aula08/firewall.tf
@@ -1,11 +1,11 @@
-resource "google_compute_firewall" "allow-http" {
+/* resource "google_compute_firewall" "allow-http" {
   name    = "default-allow-http"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["80","8080"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-} 
+}  */
--- 01-codigos-aulas/Aula08/instance.tf
+++ 01-codigos-aulas/Aula08/instance.tf
@@ -19,3 +19,25 @@ resource "google_compute_instance" "vm" {
 
   metadata_startup_script = "apt update; apt install -y nginx"
 }
+
+resource "google_compute_instance" "vm2" {
+  name         = "vm2"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "apt update; apt install -y nginx"
+}
