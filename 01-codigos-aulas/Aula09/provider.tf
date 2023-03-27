e840f4d4327d061c16da47b37ba5afe540ce1e48
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-03-27 13:22:44 -0300

    links aula 09

--- 01-codigos-aulas/Aula09/provider.tf
+++ 01-codigos-aulas/Aula09/provider.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.55.0"
     }
   }
 }
 
 provider "google" {
-    region = "us-central1"
+  project     = "terraform-lab-378618"
+  region      = "us-central1"
+  credentials = "/Users/anderson/Documents/credenciais-gcp/terraform-lab-378618-e771177c51d7.json"
 }
