585df71a968eff3d5940a225e06417ca07fb3192
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-05-29 14:30:37 -0300

    exemplos aula 12

---
+++ 01-codigos-aulas/Aula12/firewall.tf
@@ -0,0 +1,15 @@
+resource "google_compute_firewall" "allow-default" {
+  name    = "allow-default"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
