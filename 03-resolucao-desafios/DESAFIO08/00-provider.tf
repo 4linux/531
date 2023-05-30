e9909c57912517ac3a326f073caac3639dd638ce
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-05-30 16:41:39 -0300

    desfafio 08

---
+++ 03-resolucao-desafios/DESAFIO08/00-provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "terraform-lab-386821"
+  region      = "us-central1"
+  credentials = "/Users/anderson/Documents/gcp-svc/terraform-lab-386821-e8f3a13d89b9.json"
+}
